5ed8e9e9298bb301b169385af4a8952ee469d60f
imoraru-nf <79264332+imoraru-nf@users.noreply.github.com>
2023-01-12 11:28:20 +0200

    parser

--- Zoho/parser.ps1
+++ Zoho/parser.ps1
@@ -2,7 +2,6 @@ $stream_reader = New-Object System.IO.StreamReader{D:\Github\NFG\Zoho\apis.txt}
 $line_number = 1
 while (($current_line =$stream_reader.ReadLine()) -ne $null)
 {
-#Write-Host "$line_number  $current_line" *>> "D:\Github\NFG\Zoho\log.txt"
 $response = Invoke-RestMethod -Uri $current_line -Method Get
 Write-Host "$line_number  $response" *>> "D:\Github\NFG\Zoho\log.txt"
 $line_number++
