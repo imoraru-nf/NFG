b0bae05b3f1ce01b9a4d58e526caab131f6f07be
imoraru-nf <79264332+imoraru-nf@users.noreply.github.com>
2023-01-25 15:44:06 +0200

    step 2

--- Zoho/parser.ps1
+++ Zoho/parser.ps1
@@ -1,4 +1,4 @@
-$stream_reader = New-Object System.IO.StreamReader{D:\Github\NFG\Zoho\api.txt}
+$stream_reader = New-Object System.IO.StreamReader{D:\Github\NFG\Zoho\api2.txt}
 $line_number = 1
 while (($current_line =$stream_reader.ReadLine()) -ne $null)
 {
@@ -6,11 +6,11 @@ while (($current_line =$stream_reader.ReadLine()) -ne $null)
 try {  
 $response = Invoke-WebRequest -Uri $current_line
 Write-Warning  $response.content
-Write-Host "$line_number  $response.content" *>> "D:\Github\NFG\Zoho\log.txt"
+Write-Host "$line_number  $response.content" *>> "D:\Github\NFG\Zoho\log2.txt"
 }
 catch { 
 Write-Warning $_.Exception.Message
-Write-Host "$line_number  $_.Exception.Message" *>> "D:\Github\NFG\Zoho\log.txt"
+Write-Host "$line_number  $_.Exception.Message" *>> "D:\Github\NFG\Zoho\log2.txt"
 }
 $line_number++
 }
